a7b28d190e3d5056aebe5f0ff565684e9982a4c0
magnoale <alessandro.chila@playground.it>
2023-01-11 15:22:55 +0100

    fix js

--- assets/article.js
+++ assets/article.js
@@ -1,36 +1,36 @@
 (function(document) {
 
-    var selectors = {
-        navBlog: '.nav-blog'
-    };
+    // var selectors = {
+    //     navBlog: '.nav-blog'
+    // };
 
-    var navBlog = document.querySelector(selectors.navBlog);
+    // var navBlog = document.querySelector(selectors.navBlog);
 
-    window.onscroll = function() {scrollDown()};
+    // window.onscroll = function() {scrollDown()};
 
-    function scrollDown() {
-        if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-            navBlog.classList.remove("hidden")
-        } else {
-            navBlog.classList.add("hidden")
-        }
-    }
+    // function scrollDown() {
+    //     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    //         navBlog.classList.remove("hidden")
+    //     } else {
+    //         navBlog.classList.add("hidden")
+    //     }
+    // }
 
 
     console.log("init article slider")
-    // const moreArticleSlider = new Swiper("#blog-others-articles", {
-    //     slidesPerView: 3,
-    //     spaceBetween: 30,
-    //     breakpoints: {
-    //         310: {
-    //             slidesPerView: 3
-    //         },
-    //         1024: {
-    //             slidesPerView: 3,
-    //             spaceBetween: 30,
-    //         }
-    //     }
-    // }) 
+    const moreArticleSlider = new Swiper("#blog-others-articles", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        breakpoints: {
+            310: {
+                slidesPerView: 3
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            }
+        }
+    }) 
 
 })(document);
   
