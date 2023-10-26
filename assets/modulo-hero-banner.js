c03cdb2ae7f91cd49b7eb9f669ed5317057b66e6
Massimiliano Rossi <massimiliano.rossi@playground.it>
2023-10-26 10:32:04 +0200

    hero

--- assets/modulo-hero-banner.js
+++ assets/modulo-hero-banner.js
@@ -14,11 +14,19 @@
             heroBanner.classList.add('section-hero-banner-' + i);
 
             let heroBanner_height = heroBanner.offsetHeight
-            let heroScrollDown = heroBanner.querySelector('.hero-scroll-down')
 
-            heroScrollDown.addEventListener("click", function(){
-                window.scrollTo({ left: 0, top: heroBanner_height, behavior: "smooth" });
-            })
+            let heroScrollDown = heroBanner.querySelector('.hero-scroll-down')
+            let heroScrollDown_it_to_scroll = heroScrollDown.getAttribute("data-scroll")
+            
+            if (heroScrollDown_it_to_scroll) {
+                heroBanner_height = document.querySelector(heroScrollDown_it_to_scroll).offsetHeight
+            }
+
+            if (heroScrollDown) {
+                heroScrollDown.addEventListener("click", function(){
+                    window.scrollTo({ left: 0, top: heroBanner_height, behavior: "smooth" });
+                })
+            }
             
         })
     })
