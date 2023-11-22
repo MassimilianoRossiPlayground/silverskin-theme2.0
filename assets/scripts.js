0970e23ee311a8f41c7609cc855f630687f6053b
Massimiliano Rossi <massimiliano.rossi@playground.it>
2023-11-22 16:48:12 +0100

    test

--- assets/scripts.js
+++ assets/scripts.js
@@ -188,10 +188,7 @@ $( document ).ready(function() {
         //change variant name in variant label (only on async call)
         $(".variant__label-selected").text(" - " + $(this).parent().attr("data-value"));
 
-        console.log($(this).attr("data-qnt"))
         if ($(this).attr("data-qnt") < 10) {
-            console.log("minore 10")
-
             $(this).find(".qnt-alert").removeClass("hide")
             setTimeout(function(){
                 $(".variant-input label .qnt-alert").addClass("hide")
